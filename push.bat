07ea7e6aad94fe28259ccfbe83cdc0e42027d283
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 09:48:38 +0800

    Code update

--- push.bat
+++ push.bat
@@ -1,3 +1,16 @@
+set month=%date:~4,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+echo month=%month%
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+echo day=%day%
+
+set datetimef=%year%%month%%day%_%hour%%min%%secs%
+
+echo datetimef=%datetimef% > Upload.txt
+
+
+
 git add .
 git commit -m "Code update"
 git push -u origin master
