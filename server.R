a47ed1aab0a97081017df1b2bd8eedaa489b44cf
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 11:57:01 +0800

    Code update

--- server.R
+++ server.R
@@ -86,7 +86,7 @@ cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.8333333333
 
   output$cost <- renderValueBox({
 
-   costRate <- max(srvCost(), input$rateThreshold*1.50)
+   costRate <- max(1, input$rateThreshold*1.50)
 
     valueBox(
       value =  formatC(costRate, digits = 2, format = "f"),
