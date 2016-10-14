6a769aa2fa9472fb23d483d1b8d9f8c05c250661
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 10:14:26 +0800

    Code update

--- global.R
+++ global.R
@@ -60,10 +60,14 @@ downloadCount <- function(pkgStream) {
 
 # Count the total nrows of pkgStream
 timeRemain <- function(pkgStream,timeWindow,servercnt) {
-  shinySignals::reducePast(pkgStream, function(memo, value) {
+  reactive({
     remainTime <- remainTime - (as.numeric(Sys.time()) - timeWindow)*servercnt
     return(remainTime)
-  }, 0)
+  })
+  #shinySignals::reducePast(pkgStream, function(memo, value) {
+  #  remainTime <- remainTime - (as.numeric(Sys.time()) - timeWindow)*servercnt
+  #  return(remainTime)
+  #}, 0)
 }
 
 
