af3b17ec0271dca5d0f51a5e4910ee92551311e3
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 22:44:51 +0800

    Code update

--- server.R
+++ server.R
@@ -1,105 +1,164 @@
-
 library(colorspace)
-
+library(shiny)
+library(shinySignals)
+library(dplyr)
+library(shinydashboard)
+library(bubbles)
+source("bloomfilter.R")
+  
 function(input, output, session) {
-
-cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.833333333333333, 
-    fixup = TRUE, gamma = NULL, alpha = 1, ...) 
-{
+  
+  #######VARIABLES SECTION#######    
+  
+  timeRequired <- 8000 #seconds to complete
+  initialtimeRequired <- isolate(timeRequired) #initial time required to calc percent complete
+  
+  # Record the time that the session started.
+  startTime <- as.numeric(Sys.time())
+  
+  # An empty prototype of the data frame we want to create
+  prototype <- data.frame(date = character(), time = character(),size = numeric(), r_version = character(), r_arch = character(),r_os = character(), package = character(), version = character(),country = character(), ip_id = character(), received = numeric())
+  
+  packageStream <- function(session) {
+    # Connect to data source
+    sock <- socketConnection("cransim.rstudio.com", 6789, blocking = FALSE, open = "r")
+    # Clean up when session is over
+    session$onSessionEnded(function() {
+      close(sock)
+    })
+    
+    # Returns new lines
+    newLines <- reactive({
+      invalidateLater(1000, session)
+      readLines(sock)
+    })
+    
+    # Parses newLines() into data frame
+    reactive({
+      if (length(newLines()) == 0)
+        return()
+      read.csv(textConnection(newLines()), header=FALSE, stringsAsFactors=FALSE,
+               col.names = names(prototype)
+      ) %>% mutate(received = as.numeric(Sys.time()))
+    })
+  }
+  
+  # Accumulates pkgStream rows over time; throws out any older than timeWindow
+  # (assuming the presence of a "received" field)
+  packageData <- function(pkgStream, timeWindow) {
+    shinySignals::reducePast(pkgStream, function(memo, value) {
+      rbind(memo, value) %>%
+        filter(received > as.numeric(Sys.time()) - timeWindow)
+    }, prototype)
+  }
+  
+  
+  # Set the stream of session
+  pkgStream <- packageStream(session)
+  
+  # Max age of data (5 minutes)
+  maxAgeSecs <- 60 * 5
+  
+  # Set package
+  pkgData <- packageData(pkgStream, maxAgeSecs)
+  
+  # Use a bloom filter to probabilistically track the number of unique
+  # users we have seen; using bloom filter means we will not have a
+  # perfectly accurate count, but the memory usage will be bounded.
+  userCount <- function(pkgStream) {
+    # These parameters estimate that with 5000 unique users added to
+    # the filter, we'll have a 1% chance of false positive on the next
+    # user to be queried.
+    bloomFilter <- BloomFilter$new(5000, 0.01)
+    total <- 0
+    reactive({
+      df <- pkgStream()
+      if (!is.null(df) && nrow(df) > 0) {
+        # ip_id is only unique on a per-day basis. To make them unique
+        # across days, include the date. And call unique() to make sure
+        # we don't double-count dupes in the current data frame.
+        ids <- paste(df$date, df$ip_id) %>% unique()
+        # Get indices of IDs we haven't seen before
+        newIds <- !sapply(ids, bloomFilter$has)
+        # Add the count of new IDs
+        total <<- total + length(newIds)
+        # Add the new IDs so we know for next time
+        sapply(ids[newIds], bloomFilter$set)
+      }
+      total
+    })
+  }
+  
+  # Call function
+  customerCount <- userCount(pkgStream)
+  
+  # Color function
+  cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.833333333333333, 
+                  fixup = TRUE, gamma = NULL, alpha = 1, ...) 
+  {
     if (!is.null(gamma)) 
-        warning("'gamma' is deprecated and has no effect")
+      warning("'gamma' is deprecated and has no effect")
     if (n < 1L) 
-        return(character(0L))
+      return(character(0L))
     h <- rep(h, length.out = 2L)
     c <- c[1L]
     l <- rep(l, length.out = 2L)
     power <- rep(power, length.out = 2L)
     rval <- seq(1, -1, length = n)
     rval <- hex(polarLUV(L = l[2L] - diff(l) * abs(rval)^power[2L], 
-        C = c * abs(rval)^power[1L], H = ifelse(rval > 0, h[1L], 
-            h[2L])), fixup = fixup, ...)
+                         C = c * abs(rval)^power[1L], H = ifelse(rval > 0, h[1L], 
+                                                                 h[2L])), fixup = fixup, ...)
     if (!missing(alpha)) {
-        alpha <- pmax(pmin(alpha, 1), 0)
-        alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
-            width = 2L, upper.case = TRUE)
-        rval <- paste(rval, alpha, sep = "")
+      alpha <- pmax(pmin(alpha, 1), 0)
+      alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
+                      width = 2L, upper.case = TRUE)
+      rval <- paste(rval, alpha, sep = "")
     }
     return(rval)
-}
-
-
-  # pkgStream is a reactive expression that represents a stream of
-  # new package download data; up to once a second it may return a
-  # data frame of new downloads since the last update.
-  pkgStream <- packageStream(session)
+  }
   
-  # Max age of data (5 minutes)
-  maxAgeSecs <- 60 * 5
-
-  # pkgData is a reactive expression that accumulates previous
-  # values of pkgStream, discarding any that are older than
-  # maxAgeSecs.
-  pkgData <- packageData(pkgStream, maxAgeSecs)
   
-  # dlCount is a reactive expression that keeps track of the total
-  # number of rows that have ever appeared through pkgStream.
-  dlCount <- downloadCount(pkgStream)  
-
-  srvCost <- serverCost(pkgStream,1)
-
-  # usrCount is a reactive expression that keeps an approximate
-  # count of all of the unique users that have been seen since the
-  # app started.
-  usrCount <- userCount(pkgStream)
+  #######OUTPUT SECTION#######    
   
-  # Record the time that the session started.
-  startTime <- as.numeric(Sys.time())
-
-  tmRemain <- timeRemain(pkgStream,startTime,input$rateThreshold)
-  
-     
-  
-  output$rate <- renderValueBox({
-    # The downloadRate is the number of rows in pkgData since
-    # either startTime or maxAgeSecs ago, whichever is later.
-    elapsed <- as.numeric(Sys.time()) - startTime
-    downloadRate <- nrow(pkgData()) / min(maxAgeSecs, elapsed)
-    
+  output$completePercent <- renderValueBox({
+    invalidateLater(1000, session) 
     valueBox(
-      value = formatC(downloadRate, digits = 1, format = "f"),
+      value = formatC((1-timeRequired/initialtimeRequired)*100, digits = 2, format = "f"),
       subtitle = "Percent completed",
       icon = icon("percent"),
-      #color = if (downloadRate >= input$rateThreshold) "yellow" else "aqua"
       color = "yellow" 
     )
   })
   
-  output$count <- renderValueBox({
-    currentServerCnt   <- max(srvCost(), input$rateThreshold*1.50)
+  output$timetoComplete <- renderValueBox({
+    invalidateLater(1000, session) 
+    elapsed <- as.numeric(Sys.time()) - startTime #Assumes a constant value to complete the job
+    timeRequired <<- timeRequired - input$servercnt
+    timeleft <- timeRequired/(input$servercnt)
     
     valueBox(
-      value = currentServerCnt,
+      value = format(as.POSIXct('2016-01-01 00:00:00') + timeleft, "%H:%M:%S"),
       subtitle = "Time to complete",
       icon = icon("clock-o")
     )
   })
-
-  output$cost <- renderValueBox({
-
-   costRate <- max(1, input$rateThreshold*1.50)
-
+  
+  output$costPerHour <- renderValueBox({
+    invalidateLater(1000, session) 
+    costRate <- input$servercnt*1.50 #Based on Azure pricing calculator, does not include storage
+    
     valueBox(
       value =  formatC(costRate, digits = 2, format = "f"),
       subtitle = "Cost per hour",
       icon = icon("usd")
     )
   })
-
   
-  output$users <- renderValueBox({
+  output$customersScanned <- renderValueBox({
+    
     valueBox(
-      usrCount(),
-      "Unique customers",
+      value =  customerCount(),
+      subtitle = "Customers scanned",
       icon = icon("users")
     )
   })
@@ -115,7 +174,7 @@ cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.8333333333
       arrange(desc(n), tolower(package)) %>%
       # Just show the top 60, otherwise it gets hard to see
       head(60)
-      
+    
     bubbles(df$n, df$package, key = df$package,color = cx(nrow(df)) )
   })
   
@@ -130,18 +189,8 @@ cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.8333333333
       head(15)
   }, digits = 1)
   
-  output$downloadCsv <- downloadHandler(
-    filename = "cranlog.csv",
-    content = function(file) {
-      write.csv(pkgData(), file)
-    },
-    contentType = "text/csv"
-  )
   
-  output$rawtable <- renderPrint({
-    orig <- options(width = 1000)
-    print(tail(pkgData(), input$maxrows))
-    options(orig)
-  })
+  
+  
+  
 }
-
--- ui.R
+++ ui.R
@@ -1,17 +1,14 @@
+library(shiny)
+library(shinydashboard)
+library(bubbles)
+
 dashboardPage(
-  dashboardHeader(title = "Open ROADS Community",titleWidth = 350),
+  dashboardHeader(title = "Open ROADS Community"),
   dashboardSidebar(
-    sliderInput("rateThreshold", "Number of computers",
-                min = 0, max = 20, value = 3, step = 1
-    ),
+    sliderInput("servercnt", "Number of computers",min = 0, max = 20, value = 3, step = 1),
     selectInput("marketInterest","Market Interest",selected ="INVESTING", c("ACCESSORIES","ACCOUNTING","ADD","ADULT_EDUCATION","ARTS_CRAFTS","BARBECUES_GRILLING","CHRISTIANITY","EDUCATIONAL_INSTITUTIONS","ENTERTAINMENT_NEWS_CELEBRITY_SITES","ENTERTAINMENT_OTHER","FINANCIAL_PLANNING","HEALTH_LOWFAT_COOKING","INVESTING","LITERATURE_BOOKS","MOVIES","MUSIC","PRIVATE_SCHOOL","PSYCHOLOGY_PSYCHIATRY","REFERENCE_MATERIALS_MAPS","SMOKING_CESSATION","SPACE_ASTRONOMY","SPECIAL_EDUCATION","STREAMING_DOWNLOADABLE_VIDEO","TAX_PLANNING","TELEVISION","TEXT_MESSAGING_SMS","WIKIS","YEAR_712_EDUCATION")),
-    
-    sliderInput("perceivedValue", "Perceived value",
-                min = 0, max = 50, value = 40, step = 10
-    ),
-    sliderInput("costtoDeliver", "Cost to deliver",
-                min = 0, max = 50, value = 10, step = 5
-    ),
+    sliderInput("perceivedValue", "Perceived value",min = 0, max = 50, value = 40, step = 10),
+    sliderInput("costtoDeliver", "Cost to deliver",min = 0, max = 50, value = 10, step = 5),
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard"),
       menuItem("Raw data", tabName = "rawdata")
@@ -20,11 +17,12 @@ dashboardPage(
   dashboardBody(
     tabItems(
       tabItem("dashboard",
+              # Boxes need to be put in a row (or column)
               fluidRow(
-                valueBoxOutput("rate", width=3),
-                valueBoxOutput("count", width=3),
-                valueBoxOutput("cost", width=3),
-                valueBoxOutput("users", width=3)
+                valueBoxOutput("completePercent", width=3),
+                valueBoxOutput("timetoComplete", width=3),
+                valueBoxOutput("costPerHour", width=3),
+                valueBoxOutput("customersScanned", width=3)
               ),
               fluidRow(
                 box(
@@ -38,12 +36,7 @@ dashboardPage(
                   tableOutput("packageTable")
                 )
               )
-      ),
-      tabItem("rawdata",
-              numericInput("maxrows", "Rows to show", 25),
-              verbatimTextOutput("rawtable"),
-              downloadButton("downloadCsv", "Download as CSV")
-      )
-    )
+      ) #tab item
+    ) #tab items
   )
 )
