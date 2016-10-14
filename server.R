15c38e583300b2ca9173fc5dc6a29cba7e691a3e
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 11:54:12 +0800

    Code update

--- server.R
+++ server.R
@@ -75,7 +75,7 @@ cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.8333333333
   })
   
   output$count <- renderValueBox({
-    currentServerCnt   <- reactive({ as.numeric(input$rateThreshold) })  
+    currentServerCnt   <- as.numeric(input$rateThreshold)
     
     valueBox(
       value = currentServerCnt,
