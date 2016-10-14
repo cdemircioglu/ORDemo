5360e8a2533038a27afa8e720fd373d0e24303ec
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-14 11:26:22 +0800

    Code update

--- server.R
+++ server.R
@@ -58,7 +58,7 @@ cx <- function (n, h = c(-243, 360), c = 91, l = c(61, 77), power = 0.8333333333
   startTime <- as.numeric(Sys.time())
 
   #tmRemain <- timeRemain(pkgStream,startTime,input$rateThreshold)
-  tmRemain <- userColor(as.numeric(input$rateThreshold))
+  tmRemain <- userColor(as.numeric(5))
   
      
   
